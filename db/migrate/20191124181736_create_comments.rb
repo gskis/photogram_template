3662fd9b045d73950115b9ec6f8c78d94e874bbf
Raghu Betina <raghu@firstdraft.com>
2019-11-24 18:17:36 +0000

    Generate comments resource with draft:resource

---
+++ db/migrate/20191124181736_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.integer :commenter_id
+      t.integer :trip_id
+
+      t.timestamps
+    end
+  end
+end
